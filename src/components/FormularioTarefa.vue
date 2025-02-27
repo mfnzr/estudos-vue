56f1ebea518a5e4a99494c0cf016fc4e92ad57f2
Maria Fernanda Ribeiro <mfnanda.ribeiro@gmail.com>
2025-02-26 22:38:35 -0300

    componente cronometro

--- src/components/FormularioTarefa.vue
+++ src/components/FormularioTarefa.vue
@@ -6,16 +6,14 @@
             </div>
             <div class="column">
                 <div class="is-flex is-align-items-center is-justify-content-space-between">
-                    <section>
-                        <strong>00:00:00</strong>
-                    </section>
-                    <button class="button">
+                    <CronometroComponent :tempoEmSegundos="tempoEmSegundos" />
+                    <button class="button" @click="iniciar">
                         <span class="icon">
                             <i class="fas fa-play"></i>
                         </span>
                         <span>play</span>
                     </button>
-                    <button class="button">
+                    <button class="button" @click="finalizar">
                         <span class="icon">
                             <i class="fas fa-stop"></i>
                         </span>
@@ -29,8 +27,29 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import CronometroComponent from './Cronometro.vue';
 
 export default defineComponent({
     name: 'FormularioTarefa',
+    components: {
+        CronometroComponent
+    },
+    data () {
+        return {
+            tempoEmSegundos: 0,
+            cronometro: 0
+        }
+    },
+
+    methods: {
+        iniciar () {
+            this.cronometro = setInterval(() => {
+                this.tempoEmSegundos += 1;
+            }, 1000);
+        },
+        finalizar () {
+            clearInterval(this.cronometro);
+        },
+    }
 });
 </script>
